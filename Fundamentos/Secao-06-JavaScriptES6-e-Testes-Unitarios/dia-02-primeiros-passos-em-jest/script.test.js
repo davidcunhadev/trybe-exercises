74a65a7247ab1b5649fd37de4c217f3cd49084c3
¨David <davidl1199@hotmail.com>
2023-05-01 15:23:15 -0300

    Exericios dia 2

--- Fundamentos/Secao-06-JavaScriptES6-e-Testes-Unitarios/dia-02-primeiros-passos-em-jest/script.test.js
+++ Fundamentos/Secao-06-JavaScriptES6-e-Testes-Unitarios/dia-02-primeiros-passos-em-jest/script.test.js
@@ -42,34 +42,34 @@
 // })
 
 //Exercício 3//
-// const {encode, decode,} = require('./script');
+const {encode, decode,} = require('./script');
 
-// describe('Para as funções encode e decode, crie os seguintes testes em Jest:', () => {
-//   test('Teste se encode é definida', () => {
-//     expect(encode).toBeDefined();
-//   })
-//   test('Teste se decode é definida', () => {
-//     expect(decode).toBeDefined();
-//   })
-//   test('Teste se encode é uma função', () => {
-//     expect(typeof encode).toBe('function');
-//   })
-//   test('Teste se decode é uma função', () => {
-//     expect(typeof decode).toBe('function');
-//   })
-//   test('Para a função encode, teste se as vogais a, e, i, o, u são convertidas em 1, 2, 3, 4 e 5, respectivamente', () => {
-//     expect(encode("oi meu nome e David")).toEqual('43 m25 n4m2 2 D1v3d');
-//   })
-//   test('Para a função decode, teste se os números 1, 2, 3, 4 e 5 são convertidos nas vogais a, e, i, o, u, respectivamente', () => {
-//     expect(decode('43 t5d4 b2m?')).toBe('oi tudo bem?')
-//   })
-//   test('Teste se a string que é retornada pelas funções tem o mesmo número de caracteres que a string passada como parâmetro', () => {
-//     expect(encode('hello world').length).toEqual(11);
-//   })
-//   test('Teste se a string que é retornada pelas funções tem o mesmo número de caracteres que a string passada como parâmetro', () => {
-//     expect(decode('hello world').length).toEqual(11);
-//   })
-// })
+describe('Para as funções encode e decode, crie os seguintes testes em Jest:', () => {
+  test('Teste se encode é definida', () => {
+    expect(encode).toBeDefined();
+  })
+  test('Teste se decode é definida', () => {
+    expect(decode).toBeDefined();
+  })
+  test('Teste se encode é uma função', () => {
+    expect(typeof encode).toBe('function');
+  })
+  test('Teste se decode é uma função', () => {
+    expect(typeof decode).toBe('function');
+  })
+  test('Para a função encode, teste se as vogais a, e, i, o, u são convertidas em 1, 2, 3, 4 e 5, respectivamente', () => {
+    expect(encode("oi meu nome e David")).toEqual('43 m25 n4m2 2 D1v3d');
+  })
+  test('Para a função decode, teste se os números 1, 2, 3, 4 e 5 são convertidos nas vogais a, e, i, o, u, respectivamente', () => {
+    expect(decode('43 t5d4 b2m?')).toBe('oi tudo bem?')
+  })
+  test('Teste se a string que é retornada pelas funções tem o mesmo número de caracteres que a string passada como parâmetro', () => {
+    expect(encode('hello world').length).toEqual(11);
+  })
+  test('Teste se a string que é retornada pelas funções tem o mesmo número de caracteres que a string passada como parâmetro', () => {
+    expect(decode('hello world').length).toEqual(11);
+  })
+})
 
 
 
