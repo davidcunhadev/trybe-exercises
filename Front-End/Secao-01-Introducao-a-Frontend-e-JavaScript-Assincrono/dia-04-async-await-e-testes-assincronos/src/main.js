090acce249729d6fb4daa3edc60470296b3ff481
¨David <davidl1199@hotmail.com>
2023-05-24 18:33:04 -0300

    Dia 04

---
+++ Front-End/Secao-01-Introducao-a-Frontend-e-JavaScript-Assincrono/dia-04-async-await-e-testes-assincronos/src/main.js
@@ -0,0 +1,28 @@
+import Swal from 'sweetalert2';
+
+const buttonEl = document.querySelector('button');
+const inputEl = document.querySelector('input');
+const preEl = document.querySelector('pre');
+
+buttonEl.addEventListener('click', handleClick);
+
+async function handleClick() {
+  const cep = inputEl.value;
+
+  try {
+    const result = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    const data = await result.json();
+
+    if (data.erro) throw new Error('CEP inválido!');
+
+    preEl.innerHTML = JSON.stringify(data);
+    return data;
+  } catch (error) {
+    Swal.fire({
+      title: 'Error!',
+      text: 'Do you want to continue',
+      icon: 'error',
+      confirmButtonText: 'Cool'
+    })
+  }
+}
