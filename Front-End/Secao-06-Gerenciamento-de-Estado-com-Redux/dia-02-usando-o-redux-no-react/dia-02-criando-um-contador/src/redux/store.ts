820ba03e81e937656e06d3d9a16cce06566721b7
¨David <davidl1199@hotmail.com>
2023-07-12 17:28:54 -0300

    Dia 02

--- Front-End/Secao-06-Gerenciamento-de-Estado-com-Redux/dia-02-usando-o-redux-no-react/dia-02-criando-um-contador/src/redux/store.ts
+++ Front-End/Secao-06-Gerenciamento-de-Estado-com-Redux/dia-02-usando-o-redux-no-react/dia-02-criando-um-contador/src/redux/store.ts
@@ -1,5 +1,5 @@
 import { legacy_createStore as createStore } from "redux";
-import { reducer } from "./reducer";
+import { reducer } from "./reducers";
 
 export const store = createStore(reducer);
 
