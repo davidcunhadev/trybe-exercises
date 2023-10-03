558f728b54c81faa29e24065165f1bb2ad04a203
¨David <davidl1199@hotmail.com>
2023-10-03 15:47:16 -0300

    Dia 01

--- Back-End/Secao-04-Introducao-ao-NodeJS/dia-02-nodeJS-API-REST-com-Express/soccer-team-manager/src/app.js
+++ Back-End/Secao-04-Introducao-ao-NodeJS/dia-02-nodeJS-API-REST-com-Express/soccer-team-manager/src/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 
 const app = express();
-app.use(express.json());
+const apiCredentials = require('./middlewares/apiCredentials');
+const teamsRouter = require('./routes/teamsRouter');
+const validateTeam = require('./middlewares/validateTeam');
 
+app.use(express.json());
+app.use(apiCredentials);
+app.use('/teams', teamsRouter);
+
+let nextId = 3;
 const teams = [
   {
     id: 1,
@@ -16,16 +23,31 @@ const teams = [
   },
 ];
 
+// listando todos os times da API
 app.get('/teams', (req, res) => res.status(200).json({ teams }));
 
+// listando apenas um time da API pelo seu respectivo ID
+app.get('/teams/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const team = teams.find((t) => t.id === id);
+  if (team) {
+    res.json(team);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+// adicionando novo time na API
 app.post('/teams', (req, res) => {
-  const newTeam = { ...req.body };
+  const newTeam = { id: nextId, ...req.body };
   teams.push(newTeam);
+  nextId += 1;
 
   res.status(201).json({ team: newTeam });
 });
 
-app.put('/teams/:id', (req, res) => {
+// atualizando dados de um respectivo time da API
+app.put('/teams/:id', validateTeam, (req, res) => {
   const { id } = req.params;
   const { name, initials } = req.body;
 
@@ -38,6 +60,17 @@ app.put('/teams/:id', (req, res) => {
   updateTeam.name = name;
   updateTeam.initials = initials;
   res.status(200).json({ updateTeam });
+});
+
+// deletando um respectivo time da API
+app.delete('/teams/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const team = teams.find((t) => t.id === id);
+  if (team) {
+    const index = teams.indexOf(team);
+    teams.splice(index, 1);
+  }
+  res.sendStatus(204);
 });
 
 module.exports = app;
--- Back-End/Secao-04-Introducao-ao-NodeJS/dia-02-nodeJS-API-REST-com-Express/soccer-team-manager/src/server.js
+++ Back-End/Secao-04-Introducao-ao-NodeJS/dia-02-nodeJS-API-REST-com-Express/soccer-team-manager/src/server.js
@@ -1,3 +1,3 @@
 const app = require('./app');
 
-app.listen(3000, () => console.log('Servidor online na porta 3000'));
+app.listen(3001, () => console.log('Servidor online na porta 3001'));
