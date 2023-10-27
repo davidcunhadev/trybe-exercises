f90bec126d58f59f44179faa6c4a5368c6c981f5
¨David <davidl1199@hotmail.com>
2023-10-27 15:27:18 -0300

    Dia 03

---
+++ Back-End/Secao-06-ORM-e-Autenticacao/dia-03-associations-N-N-e-transactions/books-api-sequelize/src/models/Book.model.js
@@ -0,0 +1,18 @@
+const BookModel = (sequelize, DataTypes) => {
+  const Book = sequelize.define('Book',
+  {
+    id: { type: DataTypes.INTEGER, primaryKey: true },
+    name: DataTypes.STRING,
+    releaseYear: DataTypes.INTEGER,
+    totalPages: DataTypes.INTEGER,
+  },
+  {
+    timestamps: false,
+    underscored: true,
+  }
+  );
+
+  return Book;
+} 
+
+module.exports = BookModel;
---
+++ Back-End/Secao-06-ORM-e-Autenticacao/dia-03-associations-N-N-e-transactions/books-api-sequelize/src/models/User.model.js
@@ -0,0 +1,18 @@
+const UserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User',
+  {
+    id: { type: DataTypes.INTEGER, primaryKey: true },
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    age: DataTypes.INTEGER,
+  },
+  {
+    timestamps: false,
+    underscored: true,
+  },
+  );
+
+  return User;
+}
+
+module.exports = UserModel;
